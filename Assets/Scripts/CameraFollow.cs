bd4fbf67d7bd61b47e0c408a77fcd934245d0c0f
Loszi <jiozzi11@gmail.com>
2024-08-05 19:51:25 -0400

    implemented basic jumping and flipping

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
+    public GameObject skateboard;
+    private Transform target;
     private readonly float speed = 10f;
+    private SkateboardController script;
+
+    void Start() {
+        script = skateboard.GetComponent<SkateboardController>(); //can use script.on_ground to change air camera settings
+        target = skateboard.transform;
+    }
 
     void FixedUpdate()
     {
         //pos
-        Vector3 offset = new(0, 1f, -3f);
+        Vector3 offset = new(0, 1f, -2f);
         var target_position = target.TransformPoint(offset);
-        transform.position = Vector3.Lerp(transform.position, target_position, speed * Time.deltaTime); //lerp for smoothing
+        transform.position = Vector3.Lerp(transform.position, target_position, speed * Time.fixedDeltaTime); //lerp for smoothing
 
         //rotation
-        var direction = target.position - transform.position + new Vector3(0, 0.5f, 0); //vector pointing 0.5 above board
-        var rotation = Quaternion.LookRotation(direction);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+        if (script.on_ground) {
+            var direction = target.position - transform.position + new Vector3(0, 0.5f, 0); //vector pointing 0.5 above board
+            var rotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.fixedDeltaTime);   
+        }
     }
 }
--- Assets/Scripts/SkateboardController.cs
+++ Assets/Scripts/SkateboardController.cs
@@ -10,13 +10,23 @@ public class SkateboardController : MonoBehaviour
     public WheelCollider front_right;
     public WheelCollider back_left;
     public WheelCollider back_right;
+    public bool on_ground;
+    private Rigidbody rb;
     private readonly float speed = 40f;
     private readonly float max_turn_angle = 15f;
+    private readonly float pop = 100f;
+    private readonly float flip_speed = 400f;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
 
     void FixedUpdate() 
     {
         float h_input = Input.GetAxis("Horizontal");
         float v_input = Input.GetAxis("Vertical");
+        on_ground = front_left.isGrounded && front_right.isGrounded && back_left.isGrounded && back_right.isGrounded;
 
         //move foward
         front_left.motorTorque = v_input * speed;
@@ -28,5 +38,15 @@ public class SkateboardController : MonoBehaviour
         front_left.steerAngle = h_input * max_turn_angle;
         front_right.steerAngle = h_input * max_turn_angle;
 
+        //jump
+        if (Input.GetKey("space") && on_ground) {
+            rb.AddForce(Vector3.up * pop, ForceMode.Impulse);
+        }
+
+        //flipping
+        if (!on_ground) {
+            Quaternion delta_rotation = Quaternion.Euler(new Vector3(0, 0, h_input) * Time.fixedDeltaTime * -flip_speed);
+            rb.MoveRotation(rb.rotation * delta_rotation);
+        }
     }
 }
