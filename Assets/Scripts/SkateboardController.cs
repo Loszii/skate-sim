39ad72cfdfb5a021b8b506b9715ce8ab79ee5f6e
Loszi <jiozzi11@gmail.com>
2024-08-08 23:55:19 -0400

    improved kickturn and movement

--- Assets/Scripts/SkateboardController.cs
+++ Assets/Scripts/SkateboardController.cs
@@ -1,20 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 
 public class SkateboardController : MonoBehaviour
 {
+    public GameObject board_visual;
+    [HideInInspector]
+    public bool on_ground = true;
 
-    public bool on_ground = false;
     private Rigidbody rb;
-    private Vector3 velocity;
     private readonly Vector3 gravity = new(0, -50f, 0);
     private readonly float sideways_friction = 1f;
     private readonly float max_speed = 5f;
+    private readonly float kickturn_thresh = 2.5f;
     void Start()
     {   
-        velocity = new Vector3(0, 0, 0);
         rb = GetComponent<Rigidbody>();
     }
 
@@ -51,16 +53,51 @@ public class SkateboardController : MonoBehaviour
             rb.AddForce(transform.forward * v_input * Time.fixedDeltaTime * 200, ForceMode.Acceleration);
         }
 
-        //rotate
-        if (Math.Abs(local_velocity.z) > 0) {
-            rb.AddTorque(transform.up * h_input * 60 * Time.fixedDeltaTime);
+        //rotate rigid
+        Quaternion delta_rotation;
+        if (Math.Abs(local_velocity.z) < kickturn_thresh) {
+            //kickturn speed
+            delta_rotation = Quaternion.Euler(new Vector3(0, h_input, 0) * Time.fixedDeltaTime * 100);
+        } else if (local_velocity.z >= 0) {
+            delta_rotation = Quaternion.Euler(new Vector3(0, h_input, 0) * Time.fixedDeltaTime * local_velocity.z*20);
+        } else {
+            delta_rotation = Quaternion.Euler(new Vector3(0, h_input, 0) * Time.fixedDeltaTime * local_velocity.z*20);
         }
 
+        rb.MoveRotation(rb.rotation * delta_rotation);
+
+        //VISUAL EFFECTS
+
+        //kickturn
+        if (Math.Abs(h_input) > 0 && Math.Abs(local_velocity.z) < kickturn_thresh) { //kickturn
+            Quaternion kickturn_rotation = Quaternion.Euler(new Vector3(-10f, transform.eulerAngles.y, transform.eulerAngles.z));
+            board_visual.transform.rotation = Quaternion.Lerp(board_visual.transform.rotation, kickturn_rotation, 5f * Time.fixedDeltaTime);
+        } else if (Math.Abs(h_input) == 0 || Math.Abs(local_velocity.z) > kickturn_thresh) {
+            Quaternion kickturn_rotation = Quaternion.Euler(new Vector3(0f, transform.eulerAngles.y, transform.eulerAngles.z));
+            board_visual.transform.rotation = Quaternion.Lerp(board_visual.transform.rotation, kickturn_rotation, 5f * Time.fixedDeltaTime);
+        }
+
+    Transform deck = board_visual.transform.Find("Deck");
+        //board tilt
+        if (Math.Abs(h_input) > 0.1) {
+            Quaternion deck_angle = Quaternion.Euler(new Vector3(board_visual.transform.eulerAngles.x, board_visual.transform.eulerAngles.y, h_input*-10f));
+            deck.rotation = Quaternion.Lerp(deck.rotation, deck_angle, 10f * Time.fixedDeltaTime);
+        } else {
+            //reset
+            Quaternion deck_angle = Quaternion.Euler(new Vector3(board_visual.transform.eulerAngles.x, board_visual.transform.eulerAngles.y, board_visual.transform.eulerAngles.z));
+            deck.rotation = Quaternion.Lerp(deck.rotation, deck_angle, 10f * Time.fixedDeltaTime);
+        }
     }
 
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.name == "Floor") {
             on_ground = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.name == "Floor") {
+            on_ground = false;
         }
     }
 }
